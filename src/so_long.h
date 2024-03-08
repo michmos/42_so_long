f595cdadcb9af71b6beab5be630137da269feafc
Michael Moser <Michael.Moser11@web.de>
2024-03-08 18:17:33 +0100

    Add header file

---
+++ src/so_long.h
@@ -0,0 +1,12 @@
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "../external_libs/42_libs/src/libft/libft.h"
+# include "../external_libs/42_libs/src/get_next_line/get_next_line.h"
+# include "../external_libs/42_libs/src/ft_printf/ft_printf.h"
+
+// utils.c
+void	err_exit(const char *str);
+
+#endif
