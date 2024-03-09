098b8f7d57cc351eab1e764687e66542e41b6419
Michael Moser <Michael.Moser11@web.de>
2024-03-09 19:26:33 +0100

    Create read_file foo

--- src/so_long.h
+++ src/so_long.h
@@ -5,8 +5,21 @@
 # include "../external_libs/42_libs/src/libft/libft.h"
 # include "../external_libs/42_libs/src/get_next_line/get_next_line.h"
 # include "../external_libs/42_libs/src/ft_printf/ft_printf.h"
+# include <fcntl.h>
+
+typedef struct s_map
+{
+	size_t	height;
+	size_t	width;
+	char	**map;
+
+} t_map;
 
 // utils.c
 void	err_exit(const char *str);
+char	*read_file(int fd);
+
+// parsing.c
+char **parsing(char *map_path);
 
 #endif
--- src/utils.c
+++ src/utils.c
@@ -1,6 +1,6 @@
 
 #include "so_long.h"
-#include <unistd.h>
+#define	BUFFER_SIZE 128
 
 void	err_exit(const char *str)
 {
@@ -8,3 +8,32 @@ void	err_exit(const char *str)
 		write(STDERR_FILENO, str, ft_strlen(str));
 	exit(EXIT_FAILURE);
 }
+
+char	*read_file(int fd)
+{
+	char	*content;
+	char	*buffer;
+	char	*temp;
+
+	content = NULL;
+	buffer = ft_calloc(BUFFER_SIZE, sizeof(char));
+	if (!buffer)
+		return (NULL);
+	while (read(fd, buffer, BUFFER_SIZE - 1) != 0)
+	{
+		if (!*buffer)
+			return (free(buffer), free(content), NULL);
+		else
+		{
+			temp = ft_strjoin(content, buffer);
+			free(content);
+			if (!temp)
+				return (free(buffer), NULL);
+			content = temp;
+			ft_memset(buffer, 0, ft_strlen(buffer));
+		}
+	}
+	free(buffer);
+	return (content);
+}
+
