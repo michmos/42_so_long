3bab1eafdd7531722f9bcf439fbc2bddd176c930
Michael Moser <Michael.Moser11@web.de>
2024-03-09 19:26:56 +0100

    Do parsing

--- src/main.c
+++ src/main.c
@@ -1,11 +1,20 @@
 
 #include "so_long.h"
+#include <stdlib.h>
 
 int	main(int argc, char *argv[])
 {
+	t_map	map;
+
 	if (argc != 2)
 	{
 		err_exit("unvalid arguments");
+	}
+	map.map = parsing(argv[1]);
+	if (error_check(&map) == true)
+	{
+		free(map.map);
+		exit(EXIT_FAILURE);
 	}
 	return (0);
 }
