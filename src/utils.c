81e1001a8aad0a72f6384ab9ce4b39b673663c90
Michael Moser <mmoser@f1r1s19.codam.nl>
2024-03-12 10:55:21 +0100

    Add find_item_pos foo

--- src/utils.c
+++ src/utils.c
@@ -15,3 +15,30 @@ void	print_err(const char *str)
 	write(STDERR_FILENO, str, ft_strlen(str));
 	write(STDERR_FILENO, "\n", 1);
 }
+
+void	find_item_pos(char **map, char item, int *y_pos, int *x_pos)
+{
+	int y;
+	int	x;
+
+	if (!map || !*map)
+		return ;
+	y = 0;
+	while (map[y])
+	{
+		x = 0;
+		while(map[y][x])
+		{
+			if (map[y][x] == item)
+			{
+				*x_pos = x;
+				*y_pos = y;
+				return ;
+			}
+			x++;
+		}
+		y++;
+	}
+	*x_pos = -1;
+	*y_pos = -1;
+}
