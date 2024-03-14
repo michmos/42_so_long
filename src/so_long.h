39a2215c4b209a48a64c59451c24e3a087d712be
Michael Moser <mmoser@f1r1s19.codam.nl>
2024-03-14 16:27:52 +0100

    Split up utils.h

--- src/so_long.h
+++ src/so_long.h
@@ -6,6 +6,9 @@
 # include "../external_libs/42_libs/src/get_next_line/get_next_line.h"
 # include "../external_libs/42_libs/src/ft_printf/ft_printf.h"
 # include "../external_libs/MLX42/include/MLX42/MLX42.h"
+
+# include "utils.h"
+
 # include <fcntl.h>
 
 # define TEXTURE_WIDTH 64
@@ -44,50 +47,6 @@ enum e_change_name
 	ITEM = 'C',
 	EXIT = 'E'
 };
-
-// utils.c -------------------------------------------------------------------//
-
-/*
-* This function calls print_err with err_message and then exits with -1
-*
-* @param err_message The error message to print
-*/
-void	err_exit(const char *str);
-
-/* This function prints a message to stderr in the format "Error\n<message>\n"
-*
-* @param err_message The error message to print
-*/
-void	print_err(const char *err_message);
-
-/*
-* This functions searches for a character inside a 2D map
-* 
-* @param map The map to search for the item
-* @param item The item to look for
-* @param y_pos A pointer to an integer that will be initialized with the
-* y-location in case the item is found - otherwise -1
-* @param x_pos A pointer to an integer that will be initialized with the
-* x-location in case the item is found - otherwise -1
-*/
-void	find_item_pos(char **map, char item, int *y_pos, int *x_pos);
-
-/*
-* This functions frees an array of strings. Therefore the array needs to be
-* terminated with a pointer to NULL
-*
-* @param arr The array to be freed
-*/
-void	free_2d_array(char **arr);
-
-/*
-* This function creates a copy of a 2d map and returns it on success. NULL is
-* returned on failure
-*
-* @param map The map to copy
-* @param size The size of the map (excluding the terminating NULL pointer)
-*/
-char	**dup_map(char **map, size_t	size);
 
 // map_init.c ----------------------------------------------------------------//
 void	init_map(t_map *map, const char *map_path);
