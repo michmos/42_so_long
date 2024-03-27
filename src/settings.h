ec6e46652afd5a672d7b2f8acaafb941d7405852
Michael Moser <mmoser@f1r1s19.codam.nl>
2024-03-27 14:48:02 +0100

    Extract settings in separate header file

---
+++ src/settings.h
@@ -0,0 +1,30 @@
+#ifndef SETTINGS_H
+# define SETTINGS_H
+
+# define TEXTURE_WIDTH 128
+
+# define SPACE_FPS 4
+# define SPACE_TEXTURE "textures/space/water.png"
+# define WALL_FPS 4
+# define WALL_TEXTURE "textures/walls/rocks.png"
+# define PLAYER_FPS 4
+# define PLAYER_TEXTURE "textures/player/boat.png"
+# define EXIT_FPS 4
+# define EXIT_TEXTURE "textures/exit/exit.png"
+# define ITEM_FPS 2
+# define ITEM_TEXTURE "textures/items/trash.png"
+# define ENEMY_FPS 4
+# define ENEMY_TEXTURE "textures/enemies/fish.png"
+# define PLAYER_SPEED 5
+
+enum e_player_sprite_sheet
+{
+	STANDING_RIGHT = 0,
+	STANDING_LEFT,
+	MOVING_RIGHT,
+	MOVING_LEFT,
+	ENDING_RIGHT,
+	ENDING_LEFT
+};
+
+#endif
--- src/so_long.h
+++ src/so_long.h
@@ -8,22 +8,8 @@
 # include "../external_libs/MLX42/include/MLX42/MLX42.h"
 
 # include "utils/utils.h"
+# include "settings.h"
 # include <fcntl.h>
-
-# define TEXTURE_WIDTH 128
-
-# define SPACE_FPS 4
-# define SPACE_TEXTURE "textures/space/water.png"
-# define WALL_FPS 4
-# define WALL_TEXTURE "textures/walls/rocks.png"
-# define PLAYER_FPS 4
-# define PLAYER_TEXTURE "textures/player/boat.png"
-# define EXIT_FPS 4
-# define EXIT_TEXTURE "textures/exit/exit.png"
-# define ITEM_FPS 2
-# define ITEM_TEXTURE "textures/items/trash.png"
-# define ENEMY_FPS 4
-# define ENEMY_TEXTURE "textures/enemies/fish.png"
 
 enum e_entity
 {
