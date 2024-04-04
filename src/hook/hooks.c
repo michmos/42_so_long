d8270ff8aa0a2976ff078dbc85929288731b2476
Michael Moser <mmoser@f1r1s19.codam.nl>
2024-04-04 13:30:03 +0200

    Implement ending screens

--- src/hook/hooks.c
+++ src/hook/hooks.c
@@ -14,14 +14,35 @@ static char	get_entity(char **map, t_vector *position)
 
 void	my_loop_hook(void *param)
 {
-	t_game *game;
+	t_game 		*game;
+	static bool	freeze;
 
 	game = (t_game *) param;
-	move_player(game->mlx, &game->entities.player, &game->map);
-	if (get_entity(game->map.map_2d, &game->map.player_pos) == ITEM)
-		disable_item(&game->entities.item, &game->map);
-	else if (get_entity(game->map.map_2d, &game->map.player_pos) == ENEMY)
-		;
+	if (mlx_is_key_down(game->mlx, MLX_KEY_ESCAPE))
+	{
+		end_game(game, EXIT_SUCCESS);
+	}
+	if (!freeze)
+	{
+		move_player(game->mlx, &game->entities.player, &game->map);
+		move_enemies(game->mlx, &game->entities.enemy, &game->map);
+		if (get_entity(game->map.map_2d, &game->map.player_pos) == ITEM)
+		{
+			disable_item(&game->entities.item, &game->map);
+			game->map.num_items--;
+		}
+		else if (get_entity(game->map.map_2d, &game->map.player_pos) == ENEMY)
+		{
+			display_screen(game, get_rgba(0xFF, 0, 0, 70), FAILURE_MESSAGE);
+			freeze = true;
+		}
+		else if (get_entity(game->map.map_2d, &game->map.player_pos) == EXIT
+			&& game->map.num_items == 0)
+		{
+			display_screen(game, get_rgba(0, 0xFF, 0, 70), SUCCESS_MESSAGE);
+			freeze = true;
+		}
+	}
 	update_animations(game);
 }
 
