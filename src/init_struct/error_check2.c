08fef02e661f07b57b387ecca18285d611db8e4e
Michael Moser <mmoser@f1r1s19.codam.nl>
2024-04-08 11:37:02 +0200

    Adapt valid_path check for enemies

--- src/init_struct/error_check2.c
+++ src/init_struct/error_check2.c
@@ -1,6 +1,11 @@
 
 #include "../so_long.h"
 
+static int	is_valid_space(char c)
+{
+	return (c != WALL && c != 'x' && c != ENEMY);  // TODO: update for mandatory part
+}
+
 static void	cross_reachable_space(char **map, int y, int x)
 {
 	if (x == 0 || map[y][x] == '\0' || y == 0 || map[y] == NULL)
@@ -8,13 +13,13 @@ static void	cross_reachable_space(char **map, int y, int x)
 		return ;
 	}
 	map[y][x] = 'x';
-	if (map[y][x - 1] != WALL && map[y][x - 1] != 'x')
+	if (is_valid_space(map[y][x - 1]))
 		cross_reachable_space(map, y, x - 1);
-	if (map[y][x + 1] != WALL && map[y][x + 1] != 'x')
+	if (is_valid_space(map[y][x + 1]))
 		cross_reachable_space(map, y, x + 1);
-	if (map[y - 1][x] != WALL && map[y - 1][x] != 'x')
+	if (is_valid_space(map[y - 1][x]))
 		cross_reachable_space(map, y - 1, x);
-	if (map[y + 1][x] != WALL && map[y + 1][x] != 'x')
+	if (is_valid_space(map[y + 1][x]))
 		cross_reachable_space(map, y + 1, x);
 	return ;
 }
@@ -26,8 +31,8 @@ int	has_valid_path(t_map *map)
 	map_dup = dup_map(map->map_2d, map->height);
 	if (!map_dup)
 	{
-		free_2d_array((void **)map->map_2d); // TODO: check whether this is everything that needs to be freed?
-		exit(EXIT_FAILURE);
+		free_2d_array((void **)map->map_2d); // TODO: wrongly protected
+		
 	}
 	cross_reachable_space(map_dup, map->player_pos.y, map->player_pos.x);
 	if (!has_entity(map_dup, EXIT) && !has_entity(map_dup, ITEM))
