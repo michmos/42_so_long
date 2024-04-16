fc91b99a03cdd00f4f6717f53ea9f0432286ebc0
Michael Moser <Michael.Moser11@web.de>
2024-04-16 12:43:27 +0200

    Clean up code

--- src/hook/update_animations.c
+++ src/hook/update_animations.c
@@ -1,31 +1,43 @@
 
 #include "../so_long.h"
 
+static bool	update_required(double *accumulated_time, double mlx_delta_time, int fps)
+{
+	*accumulated_time += mlx_delta_time;
+	if (*accumulated_time * fps < 1)
+	{
+		return (false);
+	}
+	*accumulated_time = 0;
+	return (true);
+
+}
+
 static void	update_animation(t_entity *entity, double mlx_delta_time)
 {
 	size_t	var;
 	size_t	old_frame;
 	size_t	new_frame;
 
-	entity->delta_time += mlx_delta_time;
-	if (entity->delta_time * entity->fps < 1)
-	{
-		return;
-	}
-	entity->delta_time = 0;
-
+	if (!update_required(&entity->delta_time, mlx_delta_time, entity->fps))
+		return ;
 	old_frame = entity->current_frame;
 	new_frame = circular_increment(entity->current_frame, entity->num_frames - 1);
-	var = 0;
 	if (entity->type == PLAYER)
-		var = entity->current_variation;
-	while (var < entity->num_variations)
 	{
+		var = entity->current_variation;
 		entity->sprites[var][old_frame]->enabled = false;
 		entity->sprites[var][new_frame]->enabled = true;
-		if (entity->type == PLAYER)
-			break ;
-		var++;
+	}
+	else
+	{
+		var = 0;
+		while (var < entity->num_variations)
+		{
+			entity->sprites[var][old_frame]->enabled = false;
+			entity->sprites[var][new_frame]->enabled = true;
+			var++;
+		}
 	}
 	entity->current_frame = new_frame;
 }
